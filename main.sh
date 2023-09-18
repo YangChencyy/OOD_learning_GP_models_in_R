4898fe6ec757e124da8cebc78a735c3fb7cc342b
Yang Chen <yangchen@0587671666.vpn.umich.net>
2023-09-17 22:08:47 -0400

    update function

--- main.sh
+++ main.sh
@@ -3,10 +3,10 @@
 #SBATCH --account=sunwbgt98
 #SBATCH --job-name=main
 #SBATCH --nodes=1
-#SBATCH --tasks-per-node=1
 #SBATCH --mem=8GB
 #SBATCH --time=24:00:00
 #SBATCH --mail-user=rivachen@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --partition=standard
 
 if [ "x${PBS_NODEFILE}" != "x" ]; then
