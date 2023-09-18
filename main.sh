71092a6957c9e3e338341282aba050c466f1f18d
Yang Chen <yangchen@0587671666.vpn.umich.net>
2023-09-17 21:57:09 -0400

    initial commit

---
+++ main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --account=sunwbgt98
+#SBATCH --job-name=main
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --mem=8GB
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=rivachen@umich.edu
+#SBATCH --cpus-per-task=5
+#SBATCH --export=ALL
+#SBATCH --partition=standard
+
+if [ "x${PBS_NODEFILE}" != "x" ]; then
+    cat $PBS_NODEFILE
+fi
+
+cd /home/xysong/RaschSim/
+
+module load R
+R CMD BATCH --no--save --no--restore Rasch_HD.R
