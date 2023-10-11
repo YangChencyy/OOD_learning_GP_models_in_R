2e202c0ab6bdf2b733790f5f5eb227017648d180
Yang Chen <yangchen@0587664469.vpn.umich.net>
2023-10-10 21:38:32 -0400

    add more bash files

---
+++ main_FashionMNIST_5000_16.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --account=sunwbgt98
+#SBATCH --job-name=main
+#SBATCH --nodes=1
+#SBATCH --mem=8GB
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=rivachen@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --partition=standard
+#SBATCH --output=/home/rivachen/OOD_learning_GP_models_in_R/results_fmnist_5000_16.log
+
+module load R
+Rscript --save main.R "FashionMNIST" 5000 1000 16
---
+++ main_MNIST_5000_10.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --account=sunwbgt98
+#SBATCH --job-name=main
+#SBATCH --nodes=1
+#SBATCH --mem=8GB
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=rivachen@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --partition=standard
+#SBATCH --output=/home/rivachen/OOD_learning_GP_models_in_R/results_mnist_5000_10.log
+
+module load R
+Rscript --save main.R "MNIST" 5000 1000 10
+
