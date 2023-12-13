2ef04be341161757cd1e73562a17b52f73659226
Yang Chen <yangchen@0587664442.vpn.umich.net>
2023-12-12 23:31:32 -0500

    test cifar

--- main_Cifar_10_5000_32.sh
+++ main_Cifar_10_5000_32.sh
@@ -8,7 +8,7 @@
 #SBATCH --mail-user=rivachen@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --partition=standard
-#SBATCH --output=/home/rivachen/OOD_learning_GP_models_in_R/results_mnist_5000_32.log
+#SBATCH --output=/home/rivachen/OOD_learning_GP_models_in_R/results_cifar_5000_32.log
 
 module load R
 Rscript --save main.R "Cifar_10" 1000 1000 32    
