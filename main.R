21295c3d0fba2f1b07768ae1d118aca84a78c288
Yang Chen <yangchen@0587662460.vpn.umich.net>
2024-01-26 13:20:54 -0500

    do not use nugget

--- main.R
+++ main.R
@@ -223,21 +223,21 @@ if (InD_Dataset == "MNIST"){
 print("########")
 print(OOD_Datasets)
 
-model_fit_test(trainset = InD_Dataset, testsets = OOD_Datasets, n_tr = n_tr, n_ts = n_ts, f = f)  # Run only once, specify the training samples to use
+# model_fit_test(trainset = InD_Dataset, testsets = OOD_Datasets, n_tr = n_tr, n_ts = n_ts, f = f)  # Run only once, specify the training samples to use
 
-df = read.csv(paste0("data_", toString(f), "/", InD_Dataset, "/", OOD_Datasets[1], "_test.csv"))[,-1]
-plot <- plot_ly(data = df, x = ~df[, f+11], y = ~df[, f+12], text = ~label, mode = "markers")
+# df = read.csv(paste0("data_", toString(f), "/", InD_Dataset, "/", OOD_Datasets[1], "_test.csv"))[,-1]
+# plot <- plot_ly(data = df, x = ~df[, f+11], y = ~df[, f+12], text = ~label, mode = "markers")
 
-# Customize the appearance (optional)
-plot <- plot %>%
-  layout(
-    title = "Scatter Plot",
-    xaxis = list(title = "X1"),
-    yaxis = list(title = "X2")
-  )
+# # Customize the appearance (optional)
+# plot <- plot %>%
+#   layout(
+#     title = "Scatter Plot",
+#     xaxis = list(title = "X1"),
+#     yaxis = list(title = "X2")
+#   )
 
-# Save the plot as an HTML file
-htmlwidgets::saveWidget(plot, "scatter_plot.html")
+# # Save the plot as an HTML file
+# htmlwidgets::saveWidget(plot, "scatter_plot.html")
 
 
 
